0e6ca8f0525bd7b5fca5184122aaabb19d97a326
jule <18829523882@163.com>
2021-03-05 16:59:39 +0800

    第一次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,9 +5,11 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
- System.out.println("333334545!");
+        System.out.println("333334545!");
 
-System.out.println("HelloWorld111222222!");
+        System.out.println("HelloWorld111222222!");
+        System.out.println("git测试添加");
+
     }
-System.out.println("HelloWorld1111114!");
+
 }
